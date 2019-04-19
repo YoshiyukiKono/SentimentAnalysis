0262354bc95a43e6346d3da8e72f5a919d4751e1
YoshiyukiKono <ykono@cloudera.com>
2019-04-19 12:01:42 +0900

    Create 7.5_dataloader.py

---
+++ 7.5_dataloader.py
@@ -0,0 +1,26 @@
+def dataloader(messages, labels, sequence_length=30, batch_size=32, shuffle=False):
+    """ 
+    Build a dataloader.
+    """
+    if shuffle:
+        indices = list(range(len(messages)))
+        random.shuffle(indices)
+        messages = [messages[idx] for idx in indices]
+        labels = [labels[idx] for idx in indices]
+
+    total_sequences = len(messages)
+
+    for ii in range(0, total_sequences, batch_size):
+        batch_messages = messages[ii: ii+batch_size]
+        
+        # First initialize a tensor of all zeros
+        batch = torch.zeros((sequence_length, len(batch_messages)), dtype=torch.int64)
+        for batch_num, tokens in enumerate(batch_messages):
+            token_tensor = torch.tensor(tokens)
+            # Left pad!
+            start_idx = max(sequence_length - len(token_tensor), 0)
+            batch[start_idx:, batch_num] = token_tensor[:sequence_length]
+        
+        label_tensor = torch.tensor(labels[ii: ii+len(batch_messages)])
+        
+        yield batch, label_tensor
